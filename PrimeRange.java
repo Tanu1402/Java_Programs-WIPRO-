cb53dad6c5dbc06d15b1355938603f4ee2f2edfd
Tanu1402 <143172158+Tanu1402@users.noreply.github.com>
2025-07-29 12:54:23 +0530

    Create PrimeRange.java

---
+++ PrimeRange.java
@@ -0,0 +1,16 @@
+public class PrimeRange {
+    public static void main(String[] args) {
+        for (int num = 10; num <= 99; num++) {
+            boolean isPrime = true;
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(num);
+            }
+        }
+    }
+}
